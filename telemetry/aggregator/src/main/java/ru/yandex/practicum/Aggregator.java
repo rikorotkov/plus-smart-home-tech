510f98200c7e9fc3ffbf2cea17e6ccc06b3c1de0
Roman <googlik999@yandex.ru>
2025-10-23 16:15:33 +0300

    rename module

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AggregatorApplication {
+public class Aggregator {
 
     public static void main(String[] args) {
-        SpringApplication.run(AggregatorApplication.class, args);
+        SpringApplication.run(Aggregator.class, args);
     }
 
 }
