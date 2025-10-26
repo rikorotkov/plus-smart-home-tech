5659ec592908d159c171fcbb7264a1b922580d5b
Roman <googlik999@yandex.ru>
2025-10-26 21:05:47 +0300

    AggregationService

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/service/AggregationService.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/service/AggregationService.java
@@ -6,6 +6,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
@@ -22,6 +23,7 @@ public class AggregationService {
 
     private final KafkaTemplate<String, SpecificRecordBase> kafkaTemplate;
     private final Map<String, SensorsSnapshotAvro> snapshots = new ConcurrentHashMap<>();
+
     @Value("${topic.snapshot}")
     private String snapshotTopic;
 
@@ -29,25 +31,42 @@ public class AggregationService {
             topics = "${topic.sensor-event}",
             groupId = "${spring.kafka.consumer.group-id}"
     )
-    public void listen(SensorEventAvro event) {
-        SensorsSnapshotAvro sensorsSnapshotAvro = snapshots.computeIfAbsent(event.getHubId(), key -> SensorsSnapshotAvro.newBuilder()
-                .setHubId(key)
-                .setTimestamp(Instant.ofEpochSecond(event.getTimestamp().getEpochSecond(), event.getTimestamp().getNano()))
-                .setSensorsState(new ConcurrentHashMap<>())
-                .build());
-        SensorStateAvro sensorStateAvro = sensorsSnapshotAvro.getSensorsState().get(event.getId());
+    public void listen(SensorEventAvro event, Acknowledgment ack) {
+        try {
+            SensorsSnapshotAvro sensorsSnapshotAvro = snapshots.computeIfAbsent(event.getHubId(), key ->
+                    SensorsSnapshotAvro.newBuilder()
+                            .setHubId(key)
+                            .setTimestamp(Instant.ofEpochSecond(event.getTimestamp().getEpochSecond(), event.getTimestamp().getNano()))
+                            .setSensorsState(new ConcurrentHashMap<>())
+                            .build()
+            );
 
-        if (sensorStateAvro != null && sensorStateAvro.getData().equals(event.getPayload())) {
-            return;
+            SensorStateAvro sensorStateAvro = sensorsSnapshotAvro.getSensorsState().get(event.getId());
+
+            if (sensorStateAvro != null && sensorStateAvro.getData().equals(event.getPayload())) {
+                ack.acknowledge();
+                return;
+            }
+
+            SensorStateAvro stateAvro = SensorStateAvro.newBuilder()
+                    .setTimestamp(event.getTimestamp())
+                    .setData(event.getPayload())
+                    .build();
+
+            sensorsSnapshotAvro.getSensorsState().put(event.getId(), stateAvro);
+            sensorsSnapshotAvro.setTimestamp(Instant.ofEpochSecond(event.getTimestamp().getEpochSecond(), event.getTimestamp().getNano()));
+
+            kafkaTemplate.send(snapshotTopic, sensorsSnapshotAvro.getHubId(), sensorsSnapshotAvro).whenComplete((result, ex) -> {
+                if (ex == null) {
+                    log.info("Snapshot for hub {} sent successfully", sensorsSnapshotAvro.getHubId());
+                    ack.acknowledge();
+                } else {
+                    log.error("Failed to send snapshot for hub {}: {}", sensorsSnapshotAvro.getHubId(), ex.getMessage());
+                }
+            });
+
+        } catch (Exception e) {
+            log.error("Error processing event: {}", e.getMessage(), e);
         }
-
-        SensorStateAvro stateAvro = SensorStateAvro.newBuilder()
-                .setTimestamp(event.getTimestamp())
-                .setData(event.getPayload())
-                .build();
-
-        sensorsSnapshotAvro.getSensorsState().put(event.getId(), stateAvro);
-        sensorsSnapshotAvro.setTimestamp(Instant.ofEpochSecond(event.getTimestamp().getEpochSecond(), event.getTimestamp().getNano()));
-        kafkaTemplate.send(snapshotTopic, sensorsSnapshotAvro.getHubId(), sensorsSnapshotAvro);
     }
 }
